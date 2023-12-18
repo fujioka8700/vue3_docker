d9aa75e5738d9c363a2518c7b77a83c65ccee7fa
fujioka <fujioka8700@gmail.com>
2023-12-18 09:23:49 +0900

    [update] Use import type.

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,12 +1,17 @@
 <template>
     <div>Count: {{ count }}</div>
     <button @click="count++">Add Count</button>
+    <div>{{ user }}</div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import type { Ref } from "vue";
+import type { UserType } from "./types";
 
-const count = ref(0);
+const count: Ref<number> = ref<number>(0);
+
+const user: UserType = { id: 10, name: "tarou" };
 </script>
 
 <style scoped></style>
