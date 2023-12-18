3ae099f12d71916c481f7a2c70c8724ddafd31d9
fujioka <fujioka8700@gmail.com>
2023-12-18 12:07:44 +0900

    [update] Specify object type for generics.

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,31 +1,24 @@
 <template>
-    <div>FullName: John Doe</div>
-    <div>{{ user }}</div>
-    <div>{{ fullName }}</div>
+    <div>
+        <HelloWorld msg="Hello" :user="user" />
+    </div>
 </template>
 
 <script setup lang="ts">
-import { reactive, computed } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
 
-type User = {
+export type User = {
     firstName: string;
     lastName: string;
     age: number;
 };
 
-const user: User = reactive({
+const user = ref<User>({
     firstName: "John",
     lastName: "Doe",
     age: 25,
 });
-
-const fullName = computed<string>(() => `${user.firstName} ${user.lastName}`);
-
-const changeName: (name: string) => void = (name: string): void => {
-    user.firstName = "Jane";
-};
-
-changeName("Jane");
 </script>
 
 <style scoped></style>
