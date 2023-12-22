d768820725e9f5d27c17681b42dbf0f26ed588ca
fujioka <fujioka8700@gmail.com>
2023-12-22 17:11:26 +0900

    [update] First Vue.js

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,31 +1,12 @@
 <template>
-    <div>
-        <HelloWorld msg="Hello" :user="user" @changeName="changeName" />
-        <StringInput />
-    </div>
+    <div>Count: {{ count }}</div>
+    <button @click="count++">Add Count</button>
 </template>
 
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-import StringInput from "./components/StringInput.vue";
 import { ref } from "vue";
-import type { Ref } from "vue";
 
-export type User = {
-    firstName: string;
-    lastName: string;
-    age: number;
-};
-
-const user = ref<User>({
-    firstName: "John",
-    lastName: "Doe",
-    age: 25,
-});
-
-const changeName = (firstName: Ref<string>) => {
-    user.value.firstName = firstName.value;
-};
+const count = ref<number>(0);
 </script>
 
 <style scoped></style>
