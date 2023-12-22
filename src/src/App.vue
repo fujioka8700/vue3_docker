ea4ed2cb3c3da99bf4d9b672263f58440df01e3d
fujioka <fujioka8700@gmail.com>
2023-12-22 17:38:54 +0900

    [update] Setting computed property types.

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,9 +1,10 @@
 <template>
     <div>FullName: {{ `${user.firstName} ${user.lastName}` }}</div>
+    <div>{{ fullName }}</div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, computed } from "vue";
 
 type User = {
     firstName: string;
@@ -16,6 +17,8 @@ const user = reactive<User>({} as User);
 user.firstName = "John";
 user.lastName = "Doe";
 user.age = 25;
+
+const fullName = computed<string>(() => `${user.firstName} ${user.lastName}`);
 </script>
 
 <style scoped></style>
