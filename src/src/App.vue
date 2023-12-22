a06688e862f98a1cbe7dc3b09814d438e6df2923
fujioka <fujioka8700@gmail.com>
2023-12-22 17:22:02 +0900

    [update] Set type of user variable.

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,13 +1,18 @@
 <template>
-    <div>Count: {{ count }}</div>
-    <button @click="count++">Add Count</button>
+    <div>FullName: {{ `${user.firstName} ${user.lastName}` }}</div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import type { Ref } from "vue";
-
-const count: Ref<number> = ref(0);
+import { reactive } from "vue";
+const user: {
+    firstName: string;
+    lastName: string;
+    age: number;
+} = reactive({
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+});
 </script>
 
 <style scoped></style>
