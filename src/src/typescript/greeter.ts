a835dde722f99b94fe5416ae0d58dbd723bd7de7
fujioka <fujioka8700@gmail.com>
2023-12-25 09:56:03 +0900

    [update] generics at interface.

--- src/src/typescript/greeter.ts
+++ src/src/typescript/greeter.ts
@@ -1,21 +1,19 @@
-// const fun = <T>(args: T): T => args;
-const fun = <T extends string | number | User>(args: T): T => args;
-
-//Interface
-interface User {
-    name: string;
-    age: number;
+interface KeyPair<T, U> {
+    key: T;
+    value: U;
 }
 
-//型エイリアス
-// type User = {
-//     name: string;
-// };
+const kv1: KeyPair<number, string> = { key: 1, value: "Steve" };
 
-let result3 = fun<string>("John Doe");
+console.log(kv1);
 
-console.log(result3);
+const kv2: KeyPair<number, number> = { key: 1, value: 1000 };
 
-let result4 = fun<User>({ name: "aaa", age: 20 });
+console.log(kv2);
 
-console.log(result4);
+const kv3: KeyPair<string, string[]> = {
+    key: "10",
+    value: ["John", "Steve", "Jane"],
+};
+
+console.log(kv3);
