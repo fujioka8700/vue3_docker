5cd71fb10f436f02e79cdab09114edb19dfe73ee
fujioka <fujioka8700@gmail.com>
2023-12-25 09:52:45 +0900

    [update] Generics limit.

--- src/src/typescript/greeter.ts
+++ src/src/typescript/greeter.ts
@@ -1,5 +1,5 @@
 // const fun = <T>(args: T): T => args;
-const fun = <T extends string | number>(args: T): T => args;
+const fun = <T extends string | number | User>(args: T): T => args;
 
 //Interface
 interface User {
@@ -15,3 +15,7 @@ interface User {
 let result3 = fun<string>("John Doe");
 
 console.log(result3);
+
+let result4 = fun<User>({ name: "aaa", age: 20 });
+
+console.log(result4);
