a0a8814d16c0e03f7357ef0ead1681718f59a051
fujioka <fujioka8700@gmail.com>
2023-12-25 10:12:06 +0900

    [update] assertion 2.

--- src/src/typescript/greeter.ts
+++ src/src/typescript/greeter.ts
@@ -3,6 +3,7 @@ const getValue = (format: boolean): string | number => {
 };
 
 console.log(typeof getValue(true));
-const value = getValue(true) as string;
+// const value = getValue(true) as string;
+const value = <string>getValue(true);
 const digit = value.length;
 console.log(digit);
