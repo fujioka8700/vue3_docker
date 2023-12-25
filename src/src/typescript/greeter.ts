b586edcfc73dc9ec021b3a2cba4f58030f99975c
fujioka <fujioka8700@gmail.com>
2023-12-25 09:47:13 +0900

    [update] Generics constraints.

--- src/src/typescript/greeter.ts
+++ src/src/typescript/greeter.ts
@@ -1,8 +1,10 @@
-const fun = <T>(args: T): T => args;
+// const fun = <T>(args: T): T => args;
+const fun = <T extends string | number>(args: T): T => args;
 
 //Interface
 interface User {
     name: string;
+    age: number;
 }
 
 //型エイリアス
@@ -10,6 +12,6 @@ interface User {
 //     name: string;
 // };
 
-let result3 = fun<User>({ name: "John Doe" });
+let result3 = fun<string>("John Doe");
 
 console.log(result3);
