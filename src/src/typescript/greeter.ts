09957cbfc8fd034848a29c5ff5d23550d6da9f4e
fujioka <fujioka8700@gmail.com>
2023-12-25 11:11:23 +0900

    [update] keyof operator.

--- src/src/typescript/greeter.ts
+++ src/src/typescript/greeter.ts
@@ -1,16 +1,10 @@
-const fruits = ["apple", "banana", "lemon"] as const;
-type Fruit = (typeof fruits)[number];
-//結果
-// string[]
-const buyFruit: Fruit = "banana";
-console.log(buyFruit);
+type User = {
+    name: string;
+    age: number;
+};
 
-// const fruits = [1, 100, 2];
-// type Fruit = typeof fruits;
-//結果
-// number[]
-
-// const fruits = [100, 'banana', 'lemon'];
-// type Fruit = typeof fruits;
-//結果
-// (string|number)[]
+function getValue<T, K extends keyof T>(obj: T, key: K) {
+    return obj[key];
+}
+let user: User = { name: "John Doe", age: 24 };
+console.log(getValue<User, keyof User>(user, "name")); // "John"
