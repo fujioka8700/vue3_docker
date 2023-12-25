bc1ed8dd3864922813dbfa21d39866c914e9707f
fujioka <fujioka8700@gmail.com>
2023-12-25 10:02:49 +0900

    [update] Type Inference not possible.

--- src/src/typescript/greeter.ts
+++ src/src/typescript/greeter.ts
@@ -1,19 +1,4 @@
-type KeyPair<T, U> = {
-    key: T;
-    value: U;
-};
+let fruits = [100, "banana", "lemon"];
+const a = fruits[0].toUpperCase(); //
 
-const kv1: KeyPair<number, string> = { key: 1, value: "Steve" };
-
-console.log(kv1);
-
-const kv2: KeyPair<number, number> = { key: 1, value: 1000 };
-
-console.log(kv2);
-
-const kv3: KeyPair<string, string[]> = {
-    key: "10",
-    value: ["John", "Steve", "Jane"],
-};
-
-console.log(kv3);
+console.log(a);
