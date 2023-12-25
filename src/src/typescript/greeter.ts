869eba4d6f764643c3328cce5b0042fce3bf3f4b
fujioka <fujioka8700@gmail.com>
2023-12-25 09:23:27 +0900

    [update] type and interface.

--- src/src/typescript/greeter.ts
+++ src/src/typescript/greeter.ts
@@ -4,20 +4,20 @@ type PersonFirst = {
     height: number;
 };
 
-type PersonLast = {
+interface PersonLast {
     lastName: string;
     age: number;
     weight: number;
-};
+}
 
-type Person = PersonFirst | PersonLast;
+type Person = PersonFirst & PersonLast;
 
 const user: Person = {
     firstName: "John",
     lastName: "Doe",
     age: 10,
     weight: 60,
-    // height: 160,
+    height: 170,
 };
 
 console.log(user);
