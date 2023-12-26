d37127ac9d1d8411e35ef36466923d68f0690ec0
fujioka <fujioka8700@gmail.com>
2023-12-26 09:21:55 +0900

    [update] New Vue app.

--- src/src/main.ts
+++ src/src/main.ts
@@ -1,22 +1,6 @@
-// import "./assets/main.css";
+import './assets/main.css'
 
-// import { createApp } from "vue";
-// import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
 
-// createApp(App).mount("#app");
-
-// const person: Person = {
-//     name: "Taro",
-//     age: 23,
-// };
-
-// const count: T = 3;
-
-// console.log(person);
-// console.log(count);
-
-// import "./hello";
-
-// console.log(hello("taro"));
-
-import "./practice";
+createApp(App).mount('#app')
